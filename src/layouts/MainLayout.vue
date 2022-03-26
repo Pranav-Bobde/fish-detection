7a7a9f40c9bb898672e5f41616349ba8881946b8
Pranav-Bobde <bobdep31@gmail.com>
2022-03-26 14:17:33 +0530

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-layout view="lHh Lpr lFf" :style="bgImage">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false,
+    }
+  },
+  computed: {
+    bgImage() {
+      return {
+        backgroundImage: `url(${require('../assets/fishes.jpg')})`
+      };
+    },
+  },
+}
+</script>
